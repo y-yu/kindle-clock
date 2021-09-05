0d0a162af69ac5515d74d318c1cdabf98f22bb2e
YOSHIMURA Yuu <yyu@mental.poker>
2021-09-05 00:05:55 +0000

    Update jedis to 3.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   lazy val useCase = Nil
 
   lazy val infra = Seq(
-    "redis.clients" % "jedis" % "3.6.3",
+    "redis.clients" % "jedis" % "3.7.0",
     "com.squareup.okhttp3" % "okhttp" % "4.9.1"
   )
 
