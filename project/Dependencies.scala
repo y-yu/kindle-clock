18e5b0589ae366e9593aa93e14c2718a33c819a5
YOSHIMURA Yuu <yyu@mental.poker>
2022-01-16 00:09:57 +0000

    Update jedis to 4.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   lazy val useCase = Nil
 
   lazy val infra = Seq(
-    "redis.clients" % "jedis" % "3.8.0",
+    "redis.clients" % "jedis" % "4.0.1",
     "com.squareup.okhttp3" % "okhttp" % "4.9.3",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.1" cross CrossVersion.for3Use2_13
   )
