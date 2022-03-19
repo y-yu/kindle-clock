7afba287e4e142a6c63768a81a5a79062622eecd
Yoshimura Yuu <yyu@mental.poker>
2022-03-19 13:14:30 +0900

    Replace `BinaryJedis` to `Jedis` to avoid warnings

--- module/infra/src/main/scala/kindleclock/infra/di/InfraModule.scala
+++ module/infra/src/main/scala/kindleclock/infra/di/InfraModule.scala
@@ -5,18 +5,18 @@ import com.google.inject.TypeLiteral
 import kindleclock.infra.api.awair.AwairApiClientImpl
 import kindleclock.infra.api.natureremo.NatureRemoApiClientImpl
 import kindleclock.infra.api.openweathermap.OpenWeatherMapApiClientImpl
-import kindleclock.infra.cache.redis.BinaryJedisClientProvider
+import kindleclock.infra.cache.redis.JedisClientProvider
 import kindleclock.infra.cache.redis.RedisCacheClientJedisImpl
 import kindleclock.infra.datamodel.awair.AwairDataModel
 import kindleclock.domain.interfaces.infra.api.awair.AwairApiClient
 import kindleclock.domain.interfaces.infra.api.natureremo.NatureRemoApiClient
 import kindleclock.domain.interfaces.infra.api.openweathermap.OpenWeatherMapApiClient
 import kindleclock.domain.interfaces.infra.cache.CacheClient
-import redis.clients.jedis.BinaryJedis
+import redis.clients.jedis.Jedis
 
 class InfraModule extends AbstractModule {
   override def configure(): Unit = {
-    bind(classOf[BinaryJedis]).toProvider(classOf[BinaryJedisClientProvider])
+    bind(classOf[Jedis]).toProvider(classOf[JedisClientProvider])
     bind(new TypeLiteral[CacheClient[AwairDataModel]]() {}).to(classOf[RedisCacheClientJedisImpl])
     bind(classOf[AwairApiClient]).to(classOf[AwairApiClientImpl])
     bind(classOf[NatureRemoApiClient]).to(classOf[NatureRemoApiClientImpl])
