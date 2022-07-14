8fdce5b462a82f008ce80e797641250db2d4489c
Yoshimura Yuu <yyu@mental.poker>
2022-07-14 22:03:30 +0900

    Filter out JSON which does not have `deviceType`

--- module/infra/src/main/scala/kindleclock/infra/api/switchbot/SwitchBotApiClientImpl.scala
+++ module/infra/src/main/scala/kindleclock/infra/api/switchbot/SwitchBotApiClientImpl.scala
@@ -142,9 +142,12 @@ class SwitchBotApiClientImpl @Inject() (
 object SwitchBotApiClientImpl {
   implicit val deviceListReads: Reads[Map[SwitchBotDeviceType, String]] = {
     (__ \ "body" \ "deviceList").read[JsArray].map { case JsArray(values) =>
-      values.map { v =>
-        ((v \ "deviceType").as[SwitchBotDeviceType], (v \ "deviceId").as[String])
-      }.toMap
+      values
+        .filter(v => (v \ "deviceType").isDefined)
+        .map { v =>
+          ((v \ "deviceType").as[SwitchBotDeviceType], (v \ "deviceId").as[String])
+        }
+        .toMap
     }
   }
 
