3a63465207373a0a9ca51dbcee88d67a549d86ff
YOSHIMURA Yuu <yyu@mental.poker>
2022-09-11 00:12:54 +0000

    Update mockito-core to 4.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
       "com.google.inject" % "guice" % "5.1.0",
       scalaXmlDependency,
       "org.scalatest" %% "scalatest" % "3.2.13" % "test",
-      "org.mockito" % "mockito-core" % "4.6.1" % "test",
+      "org.mockito" % "mockito-core" % "4.8.0" % "test",
       "org.atnos" %% "eff" % effVersion,
       "org.atnos" %% "eff-monix" % effVersion
     ) ++ (if (scalaBinaryVersion.value == "3") {
