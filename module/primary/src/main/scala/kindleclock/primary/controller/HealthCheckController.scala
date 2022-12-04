35236a3a8abcc2d5c1e91b3f96a9e64573f73b5e
Yoshimura Yuu <yyu@mental.poker>
2022-12-05 02:27:53 +0900

    Add health check endpoint

---
+++ module/primary/src/main/scala/kindleclock/primary/controller/HealthCheckController.scala
@@ -0,0 +1,15 @@
+package kindleclock.primary.controller
+
+import javax.inject.Inject
+import play.api.mvc.AbstractController
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.ControllerComponents
+
+class HealthCheckController @Inject() (
+  cc: ControllerComponents
+) extends AbstractController(cc) {
+  def show: Action[AnyContent] = Action.apply { _ =>
+    Ok("")
+  }
+}
