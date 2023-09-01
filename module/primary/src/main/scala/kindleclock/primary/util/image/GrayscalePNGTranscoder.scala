433366cfb8a3bb0c3a72bf692bb212d22c4a2534
Yoshimura Yuu <yyu@mental.poker>
2023-09-01 10:58:52 +0900

    Allow external resource to load fonts

--- module/primary/src/main/scala/kindleclock/primary/util/image/GrayscalePNGTranscoder.scala
+++ module/primary/src/main/scala/kindleclock/primary/util/image/GrayscalePNGTranscoder.scala
@@ -2,8 +2,11 @@ package kindleclock.primary.util.image
 
 import java.awt.image.BufferedImage
 import org.apache.batik.transcoder.image.PNGTranscoder
+import org.apache.batik.transcoder.SVGAbstractTranscoder
 
 class GrayscalePNGTranscoder extends PNGTranscoder {
+  hints.put(SVGAbstractTranscoder.KEY_ALLOW_EXTERNAL_RESOURCES, true)
+
   override def createImage(width: Int, height: Int) =
     new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
 }
